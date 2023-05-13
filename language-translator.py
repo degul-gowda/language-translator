15894a840234117aac1b3a728a2322141b2d196a
degul-gowda <85669536+degul-gowda@users.noreply.github.com>
2023-05-13 22:19:26 +0530

    Create language-translator.py

---
+++ language-translator.py
@@ -0,0 +1,114 @@
+import random
+passlen = int(input("enter the length of password"))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
+
+Height=float(input("Enter your height in centimeters: "))
+Weight=float(input("Enter your Weight in Kg: "))
+Height = Height/100
+BMI=Weight/(Height*Height)
+print("your Body Mass Index is: ",BMI)
+if(BMI>0):
+	if(BMI<=16):
+		print("you are severely underweight")
+	elif(BMI<=18.5):
+		print("you are underweight")
+	elif(BMI<=25):
+		print("you are Healthy")
+	elif(BMI<=30):
+		print("you are overweight")
+	else: print("you are severely overweight")
+else:("enter valid details")
+
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory
+import os
+
+music_player = tkr.Tk()
+music_player.title("My Music Player")
+music_player.geometry("450x350")
+directory = askdirectory()
+os.chdir(directory)
+song_list = os.listdir()
+
+play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
+pygame.init()
+pygame.mixer.init()
+
+def play():
+    pygame.mixer.music.load(play_list.get(tkr.ACTIVE))
+    var.set(play_list.get(tkr.ACTIVE))
+    pygame.mixer.music.play()
+def stop():
+    pygame.mixer.music.stop()
+def pause():
+    pygame.mixer.music.pause()
+def unpause():
+    pygame.mixer.music.unpause()
+Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PLAY", command=play, bg="blue", fg="white")
+Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="red", fg="white")
+Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="purple", fg="white")
+Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="orange", fg="white")
+
+var = tkr.StringVar() 
+song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
+
+song_title.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+play_list.pack(fill="both", expand="yes")
+music_player.mainloop()
+
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory
+import os
+
+music_player = tkr.Tk()
+music_player.title("My Music Player")
+music_player.geometry("450x350")
+directory = askdirectory()
+os.chdir(directory)
+song_list = os.listdir()
+
+play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
+pygame.init()
+pygame.mixer.init()
+
+def play():
+    pygame.mixer.music.load(play_list.get(tkr.ACTIVE))
+    var.set(play_list.get(tkr.ACTIVE))
+    pygame.mixer.music.play()
+def stop():
+    pygame.mixer.music.stop()
+def pause():
+    pygame.mixer.music.pause()
+def unpause():
+    pygame.mixer.music.unpause()
+Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PLAY", command=play, bg="blue", fg="white")
+Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="red", fg="white")
+Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="purple", fg="white")
+Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="orange", fg="white")
+
+var = tkr.StringVar() 
+song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
+
+song_title.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+play_list.pack(fill="both", expand="yes")
+music_player.mainloop()
